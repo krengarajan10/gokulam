48a0aceff4df18e652cbea0b30f2516c76de0667
Rengarajan Kannan <Rengarajan Kannan>
2017-03-25 17:34:38 -0400

    Initial commit

---
+++ src/main/java/com/organic/shop/GokulamApplication.java
@@ -0,0 +1,12 @@
+package com.organic.shop;
+
+/**
+ * Created by Rengarajan on 3/25/2017.
+ */
+public class GokulamApplication {
+
+    public static void main(String[] arg){
+
+        System.out.println("Welcome to Gokulam");
+    }
+}
